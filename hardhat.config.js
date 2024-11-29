e913f5386d0b9876c93de6b23f8fcd75a60eb1c9
ErikLudescher <erik.ludescher@students.fhv.at>
2024-11-29 09:17:49 +0100

    Small Updates

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,5 +1,9 @@
-require("@nomiclabs/hardhat-ethers");
-require("dotentv").config();
+require("@nomicfoundation/hardhat-ethers");
+require("dotenv").config(); // Correctly load .env
+
+const url = process.env.SEPOLIA_URL;
+const privateKey = process.env.PRIVATE_KEY;
+
 module.exports = {
     solidity: "0.8.18",
     networks: {
